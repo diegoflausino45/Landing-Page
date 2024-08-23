1b90ba8d0d4265271c286824aa143ae9da9e9540
Diego Flausino <diegocflausino@gmail.com>
2024-08-23 16:34:39 -0300

    Commit pagina Home do projeto

---
+++ js/script.js
@@ -0,0 +1,65 @@
+const carouselInner = document.querySelector('.carousel-inner');
+const indicators = document.querySelectorAll('.carousel-indicators button');
+let currentSlide = 0;
+
+document.querySelector('.carousel-control-prev').addEventListener('click', () => {
+    currentSlide = (currentSlide > 0) ? currentSlide - 1 : indicators.length - 1;
+    updateCarousel();
+});
+
+document.querySelector('.carousel-control-next').addEventListener('click', () => {
+    currentSlide = (currentSlide < indicators.length - 1) ? currentSlide + 1 : 0;
+    updateCarousel();
+});
+
+indicators.forEach((indicator, index) => {
+    indicator.addEventListener('click', () => {
+        currentSlide = index;
+        updateCarousel();
+    });
+});
+
+function updateCarousel() {
+    carouselInner.style.transform = `translateX(-${currentSlide * 100}%)`;
+    indicators.forEach(indicator => indicator.classList.remove('active'));
+    indicators[currentSlide].classList.add('active');
+}
+
+function autoSlide() {
+    currentSlide = (currentSlide < indicators.length - 1) ? currentSlide + 1 : 0;
+    updateCarousel();
+}
+
+// Configura o intervalo para mudar automaticamente os slides a cada 3 segundos
+setInterval(autoSlide, 5000);
+
+
+const carouselInnerProdutos = document.querySelector('.carousel-inner-produtos');
+const items = document.querySelectorAll('.carousel-item-produtos');
+const totalItems = items.length;
+const visibleItems = 5; // Número de itens visíveis
+let currentIndex = 0;
+
+document.querySelector('.carousel-control-prev-produtos').addEventListener('click', () => {
+    if (currentIndex > 0) {
+        currentIndex--;
+    } else {
+        currentIndex = totalItems - visibleItems; // Volta ao início do carrossel
+    }
+    updateCarouselProdutos();
+});
+
+document.querySelector('.carousel-control-next-produtos').addEventListener('click', () => {
+    if (currentIndex < totalItems - visibleItems) {
+        currentIndex++;
+    } else {
+        currentIndex = 0; // Volta ao início do carrossel
+    }
+    updateCarouselProdutos();
+});
+
+function updateCarouselProdutos() {
+    const translateXValue = -(currentIndex * (100 / visibleItems));
+    carouselInnerProdutos.style.transform = `translateX(${translateXValue}%)`;
+}
+
